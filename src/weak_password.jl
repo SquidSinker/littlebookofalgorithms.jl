c2c69fcbb37a4ee1fddaea92240fb82810e26260
Squid Sinker <42100424+SquidSinker@users.noreply.github.com>
2020-07-14 10:18:44 +0100

    Challenge 24

--- src/weak_password.jl
+++ src/weak_password.jl
@@ -2,10 +2,22 @@ function weak_password(password)
     weak_passwords = ["password", "qwerty", "hello123", "letmein", "123456"]
     for pass in weak_passwords
         if password == pass
-            println("WEAK")
-            break
-        else
-            println("You have satisfied the necessary")
+            println("WEAK: Weak password")
+            return Nothing
         end
+    end
+    is_num = false
+    for char in password
+        if isnumeric(char) == true
+            is_num = true
+        end
+    end
+    if is_num = false
+        println("WEAK: No numerics")
+        return Nothing
+    end
+    if length(password) < 8
+        println("WEAK: Too short")
+        return Nothing
     end
 end
